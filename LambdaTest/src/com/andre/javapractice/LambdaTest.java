1233c5c4bba4334c42a804d4e68000784144c464
yefxyz <yefxyz@outlook.com>
2016-11-23 20:43:12 +0800

    LambdaTest

---
+++ LambdaTest/src/com/andre/javapractice/LambdaTest.java
@@ -0,0 +1,41 @@
+package com.andre.javapractice;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Predicate;
+
+/**
+ * LambdaTest.
+ * 
+ * @author Andre
+ */
+public class LambdaTest {
+
+	private static List<Person> roster;
+	static {
+		roster = new ArrayList<>();
+		roster.add(new Person("Marvin", LocalDate.parse("1980-05-01"), Person.Sex.MALE, "13874689281", "marvin@sina.com"));
+		roster.add(new Person("Steve", LocalDate.parse("1997-08-23"), Person.Sex.MALE, "18638659689", "steve01@163.com"));
+		roster.add(new Person("Jane", LocalDate.parse("1995-10-14"), Person.Sex.FEMALE, "18761616688", "janeAnne@outlook.com"));
+		roster.add(new Person("Dolores", LocalDate.parse("2000-01-31"), Person.Sex.FEMALE, "13610102846", "doloresWong@gmail.com"));
+		roster.add(new Person("William", LocalDate.parse("1992-02-14"), Person.Sex.MALE, "13910563827", "williamBlack@westworld.com"));
+	}
+
+	public static void printPersonsWithPredicate(List<Person> roster, Predicate<Person> tester) {
+		for (Person p : roster) {
+			if (tester.test(p)) {
+				p.printPerson();
+			}
+		}
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		printPersonsWithPredicate(roster, p -> p.getGender() == Person.Sex.MALE && p.getAge() >= 18 && p.getAge() < 25);
+
+	}
+
+}
---
+++ LambdaTest/src/com/andre/javapractice/Person.java
@@ -0,0 +1,78 @@
+package com.andre.javapractice;
+
+import java.time.LocalDate;
+
+public class Person {
+
+	public enum Sex {
+		MALE, FEMALE
+	}
+
+	private String name;
+	
+	private LocalDate birthday;
+	
+	private Sex gender;
+
+	private String phoneNumber;
+
+	private String emailAddress;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public LocalDate getBirthday() {
+		return birthday;
+	}
+
+	public void setBirthday(LocalDate birthday) {
+		this.birthday = birthday;
+	}
+
+	public Sex getGender() {
+		return gender;
+	}
+
+	public void setGender(Sex gender) {
+		this.gender = gender;
+	}
+
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
+	public String getEmailAddress() {
+		return emailAddress;
+	}
+
+	public void setEmailAddress(String emailAddress) {
+		this.emailAddress = emailAddress;
+	}
+
+	public Person(String name, LocalDate birthday, Sex gender, String phoneNumber, String email) {
+		this.name = name;
+		this.birthday = birthday;
+		this.gender = gender;
+		this.phoneNumber = phoneNumber;
+		this.emailAddress = email;
+	}
+
+	public int getAge() {
+		LocalDate now = LocalDate.now();
+		return now.getYear() - birthday.getYear();
+	}
+
+	public void printPerson() {
+		System.out.println("Name: " + name + " Gender: " + gender + " Age: " + getAge() + " Mobile: " + phoneNumber
+				+ " email: " + emailAddress);
+	}
+}
